9ac1b33db2e67518c5574ebab318a48a65362a91
Isis Vieira <157911799+izyVieira@users.noreply.github.com>
2024-02-16 14:01:04 +0000

    ...

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -3,97 +3,113 @@ import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
-let TAB_DATA = [
-  {
-    title: "Skills",
-    id: "skills",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
-        <li>React</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Education",
-    id: "education",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
-      </ul>
-    ),
-  },
-  {
-    title: "Certifications",
-    id: "certifications",
-    content: (
-      <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
-      </ul>
-    ),
-  },
-];
-
 let AboutSection = () => {
-  let [tab, setTab] = useState("skills");
-  let [isPending, startTransition] = useTransition();
 
-  let handleTabChange = (id) => {
-    startTransition(() => {
-      setTab(id);
-    });
-  };
+    let linguagens = [
+        { nome: "HTML",  porc: '65%' },
+        { nome: "CSS", porc: '55%' },
+        { nome: "React", porc: '70%' },
+        { nome: "Next.js", porc: '75%' },
+        { nome: "Node.js",  porc: undefined },
+        { nome: "JavaScript",  porc: '80%' },
+        { nome: "MongoDB",  porc: '57%' },
+        { nome: "Figma",  porc: '48%' },
+        { nome: "Tailwindcss",  porc: '40%' },
+    ];
 
-  return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab).content}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    let TAB_DATA = [
+        {
+            title: "Skills",
+            id: "skills",
+                content: (
+                    <>
+                        <ul className="list-disc pl-2">
+                            {linguagens.map((linguagem, index) => (
+                                <div key={index} className="">
+                                    <li>{linguagem.nome}</li>
+                                </div>
+                            ))}
+                        </ul>
+                        <div className="border-t border-gray-400 mt-4 pt-4">
+                            {linguagens.map((linguagem, index) => (
+                                linguagem.porc && (
+                                    <div  key={index} className="mt-2 bg-gray-600 rounded-full">
+                                        <div className="text-xs font-medium bg-purple-600 text-center p-0.5 leading-none rounded-full" 
+                                            style={{width: linguagem?.porc}}>
+                                            {linguagem.nome} - {linguagem.porc}
+                                        </div>
+                                    </div>
+                                )
+                            ))}
+                        </div>
+                    </>
+                    
+                ),
+        },
+        {
+            title: "Education",
+            id: "education",
+            content: (
+            <ul className="list-disc pl-2">
+                <li>IFSP - Instituto Federal de Educação, Ciência e Tecnologia de São Paulo, Campus Birigui</li>
+            </ul>
+            ),
+        },
+    ];
+
+    let [tab, setTab] = useState("skills");
+    let [isPending, startTransition] = useTransition();
+
+    let handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+
+    return (
+        <section className="text-white" id="about">
+            <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+
+                <Image 
+                    src="/images/imgfoto.jpg" 
+                    width={400} 
+                    height={400} 
+                />
+
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+
+                    <h2 className="text-4xl font-bold text-white mb-4">Sobre Mim</h2>
+
+                    <p className="text-base lg:text-lg">
+                        Especialização em Desenvolvimento Web: Proficiência em front-end e back-end. 
+                        Tecnologias dominadas: Node.js, JavaScript, HTML/CSS, React, Next.js, MongoDB e MySQL.
+                    </p>
+                    
+                    <div className="flex flex-row justify-start mt-8">
+                        <TabButton
+                            selectTab={() => handleTabChange("skills")}
+                            active={tab === "skills"}
+                            >
+                            {" "}
+                            Skills{" "}
+                        </TabButton>
+
+                        <TabButton
+                            selectTab={() => handleTabChange("education")}
+                            active={tab === "education"}
+                            >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                    </div>
+                    
+                    <div className="mt-8">
+                        {TAB_DATA.find((t) => t.id === tab).content}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default AboutSection;
